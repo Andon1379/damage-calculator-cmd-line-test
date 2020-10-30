d2fb4e8caaa97b538e7c0677453d6de053ebd5bb
Andon1379 <afcb1379@gmail.com>
2020-10-30 07:55:22 -0400

    First commit from cmd line

---
+++ Desktop/coding/damage calculator/README.md
@@ -0,0 +1,25 @@
+this is a damage calculator for minecraft that i am writing in python. 
+
+it is not finished, not by a long shot.
+to do:
+    create dictionaries for: 
+	damage done by melee weapons (sword, axe, trident, pickaxe, anything that says it does more damage)
+	damage done by ranged weapons (bow, crossbow, trident) 
+	    account for diffrent ammunition for bow/crossbow (arrow, firework (crossbow), tipped arrows)
+account for charge of strike
+account if it was a crit or not
+account for enchantments on weapons (sharpness, smite, power)
+    account for type of defender (zombie, enderman, dragon, player) 
+    account for fire damage applied over time by fire aspect and flame
+account for armor points that the defender has
+    account for enchantments on armor that the defender has (proc, fire proc, proj. proc)
+    account for armor toughness on defender
+account for potion effects (strength, weakness) 
+
+plan for orginization: 
+folder for dictionaries 
+   dictionary for armor
+   dictionary for defending entities 
+   dictionary for attacking entities
+   dictionary for weapons
+   dictionary for enchantments 
---
+++ Desktop/coding/damage calculator/dict/armor_dict.py
@@ -0,0 +1,51 @@
+armorptsdict = {
+    "leather helmet" : 1,
+    "leather chestplate" : 3,
+    "leather leggings" : 2,
+    "leather boots" : 1,
+    "golden helmet" : 2,
+    "golden chestplate" : 5,
+    "golden leggings" : 3,
+    "golden boots" : 1,
+    "chainmail helmet" : 2,
+    "chainmail chestplate" : 5,
+    "chainmail leggings" : 4,
+    "chainmail boots" : 1,
+    "iron helmet" : 2,
+    "iron chestplate" : 6,
+    "iron leggings" : 5,
+    "iron boots" : 2,
+    "diamond helmet" : 3,
+    "diamond chestplate" : 8,
+    "diamond leggings" : 6,
+    "diamond boots" : 3,
+    "netherite helmet" : 3,
+    "netherite chestplate" : 8,
+    "netherite leggings" : 6,
+    "netherite boots" : 3,
+    "turtle shell" : 2
+    }
+armortoughdict = {
+    "diamond helmet" : 2,
+    "diamond chestplate" : 2,
+    "diamond leggings" : 2,
+    "diamond boots" : 2,
+    "netherite helmet" : 3,
+    "netherite chestplate" : 3,
+    "netherite leggings" : 3,
+    "netherite boots" : 3
+    }
+
+"""
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+"""
+armordict = {
+    "armorpts" : armorptsdict,
+    "armortough" : armortoughdict
+    }
